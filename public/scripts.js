77063013823ba13a290713d70883c556d70b179c
brndhn <brendhainreid@gmail.com>
2017-11-29 20:14:13 -0500

    Required child_process for server.js. Attached /open to event listener in scripts.js with ajax.

--- public/scripts.js
+++ public/scripts.js
@@ -1,9 +1,11 @@
 var button = document.querySelector("#clickMe");
 
 
-
 button.addEventListener("click",function(e) {
     e.preventDefault();
     window.open('https://secure.helpscout.net/dashboard/');
     window.open('https://app.asana.com/');
+    $.ajax({
+      url: "/open"
+    });
 });
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
+const { exec } = require('child_process');
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/open', function (req, res) {
+  res.send('I\'m openning Slack')
+
+exec('open /Applications/Slack.app/Contents/MacOS/Slack', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
 })
 
 app.listen(3000, function () {
